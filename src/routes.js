8b66d65deb34089caf7225855d2a69e4ad29c8c3
benjamin-cota <benjamin.cota@uabc.edu.mx>
2021-10-20 23:55:07 -0700

    Modificacion de menu lateral

--- src/routes.js
+++ src/routes.js
@@ -19,27 +19,27 @@
 import Dashboard from "@material-ui/icons/Dashboard";
 import Person from "@material-ui/icons/Person";
 import LibraryBooks from "@material-ui/icons/LibraryBooks";
-import BubbleChart from "@material-ui/icons/BubbleChart";
-import LocationOn from "@material-ui/icons/LocationOn";
-import Notifications from "@material-ui/icons/Notifications";
-import Unarchive from "@material-ui/icons/Unarchive";
-import Language from "@material-ui/icons/Language";
+//import BubbleChart from "@material-ui/icons/BubbleChart";
+//import LocationOn from "@material-ui/icons/LocationOn";
+//import Notifications from "@material-ui/icons/Notifications";
+//import Unarchive from "@material-ui/icons/Unarchive";
+//import Language from "@material-ui/icons/Language";
 // core components/views for Admin layout
 import DashboardPage from "views/Dashboard/Dashboard.js";
 import UserProfile from "views/UserProfile/UserProfile.js";
 import TableList from "views/TableList/TableList.js";
 import Typography from "views/Typography/Typography.js";
-import Icons from "views/Icons/Icons.js";
-import Maps from "views/Maps/Maps.js";
-import NotificationsPage from "views/Notifications/Notifications.js";
-import UpgradeToPro from "views/UpgradeToPro/UpgradeToPro.js";
+//import Icons from "views/Icons/Icons.js";
+//import Maps from "views/Maps/Maps.js";
+//import NotificationsPage from "views/Notifications/Notifications.js";
+//import UpgradeToPro from "views/UpgradeToPro/UpgradeToPro.js";
 // core components/views for RTL layout
-import RTLPage from "views/RTLPage/RTLPage.js";
+//import RTLPage from "views/RTLPage/RTLPage.js";
 
 const dashboardRoutes = [
   {
     path: "/dashboard",
-    name: "Dashboard",
+    name: "Foros Sociales",
     rtlName: "لوحة القيادة",
     icon: Dashboard,
     component: DashboardPage,
@@ -47,7 +47,7 @@ const dashboardRoutes = [
   },
   {
     path: "/user",
-    name: "User Profile",
+    name: "Mi Perfil",
     rtlName: "ملف تعريفي للمستخدم",
     icon: Person,
     component: UserProfile,
@@ -55,7 +55,7 @@ const dashboardRoutes = [
   },
   {
     path: "/table",
-    name: "Table List",
+    name: "Foros de Actividades",
     rtlName: "قائمة الجدول",
     icon: "content_paste",
     component: TableList,
@@ -63,13 +63,13 @@ const dashboardRoutes = [
   },
   {
     path: "/typography",
-    name: "Typography",
+    name: "Chat",
     rtlName: "طباعة",
     icon: LibraryBooks,
     component: Typography,
     layout: "/admin",
   },
-  {
+  /*{
     path: "/icons",
     name: "Icons",
     rtlName: "الرموز",
@@ -108,7 +108,7 @@ const dashboardRoutes = [
     icon: Unarchive,
     component: UpgradeToPro,
     layout: "/admin",
-  },
+  },*/
 ];
 
 export default dashboardRoutes;
